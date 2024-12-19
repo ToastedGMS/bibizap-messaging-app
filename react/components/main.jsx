1da4c30173373c756b94a550d8647357daa9d85b
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-19 20:27:49 -0300

    added login route and globally managed state

--- react/components/main.jsx
+++ react/components/main.jsx
@@ -1,9 +1,25 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
+import Login from './Login.jsx';
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-);
+const Root = () => {
+	const [accessToken, setAccessToken] = useState(null);
+
+	return (
+		<StrictMode>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route
+						path="/login"
+						element={<Login tokenState={{ accessToken, setAccessToken }} />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</StrictMode>
+	);
+};
+
+createRoot(document.getElementById('root')).render(<Root />);
