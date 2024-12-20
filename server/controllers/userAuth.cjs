012fa1c0de969be55f3db5766fdbe7787ec8cec0
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-20 16:52:13 -0300

    update error handling

--- server/controllers/userAuth.cjs
+++ server/controllers/userAuth.cjs
@@ -15,10 +15,6 @@ async function createUser(email, password, bio, dp, username) {
 
 	try {
 		const newUser = await dbCreateUser({ userInfo });
-
-		if (!newUser) {
-			throw new Error('User creation failed: No user returned from database');
-		}
 		return newUser;
 	} catch (error) {
 		console.error('Error accessing database', error);
