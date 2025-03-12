1d9f8f2adda52acd1da44b0eae16a8917ff4985e
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-03-12 14:57:21 -0300

    remove prop-drilling from user profile and login  - add context to handle the userInfo

--- react/components/main.jsx
+++ react/components/main.jsx
@@ -1,21 +1,17 @@
 import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-	BrowserRouter,
-	Routes,
-	Route,
-	useNavigate,
-	Navigate,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
 import Logout from './Logout.jsx';
-import UserProfile from './UserProfile.jsx';
+import UserProfile from './user/UserProfile.jsx';
 import UpdateUser from './UpdateUser.jsx';
 import Friends from './Friends.jsx';
 import ChatCollection from './ChatCollection.jsx';
 import Chat from './Chat.jsx';
 import { io } from 'socket.io-client';
 import Signup from './Signup.jsx';
+import Home from './Home.jsx';
+import UserContext from '../context/UserContext.jsx';
 const socket = io('http://192.168.1.28:4000');
 
 const Root = () => {
@@ -26,87 +22,89 @@ const Root = () => {
 
 	return (
 		<StrictMode>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Navigate to="/login" />} />
-					<Route path="/signup" element={<Signup />} />
+			<UserContext.Provider value={{ userInfo, setUserInfo }}>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/signup" element={<Signup />} />
 
-					<Route
-						path="/login"
-						element={
-							<Login
-								tokenState={{ accessToken, setAccessToken }}
-								setUserInfo={setUserInfo}
-								errorState={{ errorMessage, setErrorMessage }}
-								socketIdState={{ socketID, setSocketID }}
-								socket={socket}
-							/>
-						}
-					/>
-					<Route
-						path="/logout"
-						element={<Logout setAccessToken={setAccessToken} />}
-					/>
+						<Route
+							path="/login"
+							element={
+								<Login
+									tokenState={{ accessToken, setAccessToken }}
+									setUserInfo={setUserInfo}
+									errorState={{ errorMessage, setErrorMessage }}
+									socketIdState={{ socketID, setSocketID }}
+									socket={socket}
+								/>
+							}
+						/>
+						<Route
+							path="/logout"
+							element={<Logout setAccessToken={setAccessToken} />}
+						/>
 
-					<Route
-						path="/user"
-						element={
-							<UserProfile
-								userInfoState={{ userInfo, setUserInfo }}
-								setErrorMessage={setErrorMessage}
-							/>
-						}
-					/>
+						<Route
+							path="/user"
+							element={
+								<UserProfile
+									userInfoState={{ userInfo, setUserInfo }}
+									setErrorMessage={setErrorMessage}
+								/>
+							}
+						/>
 
-					<Route
-						path="/user/update"
-						element={
-							<UpdateUser
-								userInfoState={{ userInfo, setUserInfo }}
-								setErrorMessage={setErrorMessage}
-								accessToken={accessToken}
-							/>
-						}
-					/>
+						<Route
+							path="/user/update"
+							element={
+								<UpdateUser
+									userInfoState={{ userInfo, setUserInfo }}
+									setErrorMessage={setErrorMessage}
+									accessToken={accessToken}
+								/>
+							}
+						/>
 
-					<Route
-						path="/user/friendships"
-						element={
-							<Friends
-								userInfoState={{ userInfo, setUserInfo }}
-								setErrorMessage={setErrorMessage}
-								accessToken={accessToken}
-								socketID={socketID}
-								socket={socket}
-							/>
-						}
-					/>
+						<Route
+							path="/user/friendships"
+							element={
+								<Friends
+									userInfoState={{ userInfo, setUserInfo }}
+									setErrorMessage={setErrorMessage}
+									accessToken={accessToken}
+									socketID={socketID}
+									socket={socket}
+								/>
+							}
+						/>
 
-					<Route
-						path="/chats"
-						element={
-							<ChatCollection
-								accessToken={accessToken}
-								userInfoState={{ userInfo, setUserInfo }}
-								setErrorMessage={setErrorMessage}
-							/>
-						}
-					/>
+						<Route
+							path="/chats"
+							element={
+								<ChatCollection
+									accessToken={accessToken}
+									userInfoState={{ userInfo, setUserInfo }}
+									setErrorMessage={setErrorMessage}
+								/>
+							}
+						/>
 
-					<Route
-						path="/chats/chat"
-						element={
-							<Chat
-								accessToken={accessToken}
-								userInfoState={{ userInfo, setUserInfo }}
-								setErrorMessage={setErrorMessage}
-								socket={socket}
-								socketID={socketID}
-							/>
-						}
-					/>
-				</Routes>
-			</BrowserRouter>
+						<Route
+							path="/chats/chat"
+							element={
+								<Chat
+									accessToken={accessToken}
+									userInfoState={{ userInfo, setUserInfo }}
+									setErrorMessage={setErrorMessage}
+									socket={socket}
+									socketID={socketID}
+								/>
+							}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</UserContext.Provider>
 		</StrictMode>
 	);
 };
--- react/components/user/UserProfile.jsx
+++ react/components/user/UserProfile.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchUserInfo } from './FetchUserInfo.cjs';
 import UserCard from './UserCard';
+import UserContext from '../../context/UserContext';
 
-export default function UserProfile({ userInfoState, setErrorMessage }) {
+export default function UserProfile({ setErrorMessage }) {
 	const navigate = useNavigate();
-	const { userInfo, setUserInfo } = userInfoState;
+	const { userInfo, setUserInfo } = useContext(UserContext);
 
 	useEffect(() => {
 		if (userInfo === null) {
